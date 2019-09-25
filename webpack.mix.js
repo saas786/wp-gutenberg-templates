d253281b29e1da5bedb549f486585bc3dd199205
saasfreelancer <syed.sayal.shah@gmail.com>
2019-09-25 17:28:52 +0500

    webpack config add sourcemaps & version manifest

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,29 @@
 const mix = require('laravel-mix');
 
+/*
+ * Builds sources maps for assets.
+ *
+ * @link https://laravel.com/docs/5.6/mix#css-source-maps
+ */
+mix.sourceMaps();
+
+/*
+ * Versioning and cache busting. Append a unique hash for production assets. If
+ * you only want versioned assets in production, do a conditional check for
+ * `mix.inProduction()`.
+ *
+ * @link https://laravel.com/docs/5.6/mix#versioning-and-cache-busting
+ */
+mix.version();
+
 // mix.sass('src/style.scss', 'dist/');
 mix.react('src/index.js', 'dist/');
 
 mix.babelConfig({
   'plugins': [
-    [ '@wordpress/babel-plugin-makepot', { 'output': 'languages/javascript.pot' } ],
+    [ '@wordpress/babel-plugin-makepot', {
+      'output': 'languages/javascript.pot'
+    } ],
   ],
 });
 
